20bd3570cb6f6f9132a3f5a44928d32514d19378
David Moreau <dmoreau@lamobilery.fr>
2021-10-08 11:29:12 +0200

    feat: check rentals durations

--- domain/Validators/RentalCreationValidator.test.ts
+++ domain/Validators/RentalCreationValidator.test.ts
@@ -19,3 +19,61 @@ it('validates when there is no current or upcoming rental', () => {
 
   expect(validationErrors).toHaveLength(0)
 })
+
+it('fails when startDate >= endDate', () => {
+  const validationErrors = RentalCreationValidator.validate([], {
+    vehicle: {
+      id: '1',
+      name: 'aName',
+      type: VehicleType.BICYCLE,
+    },
+    client: {
+      id: '1',
+      firstName: 'aFirstName',
+      lastName: 'aLastName',
+    },
+    startDate: new Date('2021-01-02'),
+    endDate: new Date('2021-01-02'),
+  })
+
+  expect(validationErrors).toContainEqual('startDate must be < endDate')
+})
+
+describe('rentals duration', () => {
+  it('fails when duration < 24h', () => {
+    const validationErrors = RentalCreationValidator.validate([], {
+      vehicle: {
+        id: '1',
+        name: 'aName',
+        type: VehicleType.BICYCLE,
+      },
+      client: {
+        id: '1',
+        firstName: 'aFirstName',
+        lastName: 'aLastName',
+      },
+      startDate: new Date('2021-01-02Z10:00:00'),
+      endDate: new Date('2021-01-02Z20:00:00'),
+    })
+
+    expect(validationErrors).toContainEqual('rentals must be 1d minimum')
+  })
+  it('fails when duration > 7d', () => {
+    const validationErrors = RentalCreationValidator.validate([], {
+      vehicle: {
+        id: '1',
+        name: 'aName',
+        type: VehicleType.BICYCLE,
+      },
+      client: {
+        id: '1',
+        firstName: 'aFirstName',
+        lastName: 'aLastName',
+      },
+      startDate: new Date('2021-01-01'),
+      endDate: new Date('2021-01-10'),
+    })
+
+    expect(validationErrors).toContainEqual('rentals must be 7d maximum')
+  })
+})
--- domain/Validators/RentalCreationValidator.ts
+++ domain/Validators/RentalCreationValidator.ts
@@ -3,9 +3,33 @@ import { Rental } from '../Types/Rental'
 export default class RentalCreationValidator {
   public static validate(
     _currentAndUpcomingRentals: Rental[],
-    _rentalToCreate: Rental
+    rentalToCreate: Rental
   ): ValidationError[] {
-    return []
+    let validationErrors: string[] = []
+
+    const { startDate, endDate } = rentalToCreate
+
+    if (!this.endDateIsAfterStartDate(startDate, endDate)) {
+      validationErrors.push('startDate must be < endDate')
+    }
+
+    if (this.isLessThan24Hours(startDate, endDate)) {
+      validationErrors.push('rentals must be 1d minimum')
+    }
+
+    if (endDate.getTime() - startDate.getTime() > 7 * 24 * 60 * 60 * 1000) {
+      validationErrors.push('rentals must be 7d maximum')
+    }
+
+    return validationErrors
+  }
+
+  private static isLessThan24Hours(startDate: Date, endDate: Date): boolean {
+    return endDate.getTime() - startDate.getTime() < 24 * 60 * 60 * 1000
+  }
+
+  private static endDateIsAfterStartDate(startDate: Date, endDate: Date): boolean {
+    return endDate > startDate
   }
 }
 
